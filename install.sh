2ac787e5c6f697d1034c725d9398c16202167c31
Shawn Welch <shrimpwagon@gmail.com>
2024-04-30 13:00:34 -0400

    Fixes

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ find storage/framework -maxdepth 1 -type d -exec chmod 777 {} +
 
 chmod 777 storage/logs
 
+setfacl -m "default:group::rw" storage/logs
+
 chmod 777 bootstrap/cache
 
 composer --ignore-platform-reqs install
