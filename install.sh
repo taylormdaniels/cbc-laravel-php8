7180eb07fe71f189a8d0d1420182acf9ca6461a1
Shawn Welch <shrimpwagon@gmail.com>
2024-04-30 12:27:27 -0400

    Updates

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+find storage/framework -maxdepth 1 -type d -exec chmod 777 {} +
+
+chmod 777 storage/logs
+
+chmod 777 bootstrap/cache
+
+composer --ignore-platform-reqs install
+
+npm install
