3584249cf5ffa9ce22a7bc0359652e271179f25c
Taylor <taylormdaniels1@gmail.com>
2024-08-13 17:51:48 -0400

    added some migrations

---
+++ database/migrations/2024_08_13_192745_create_purchase_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vendor_id');
+            $table->bigInteger('order_number');
+            $table->dateTime('purchase_date');
+            $table->string('sku');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_orders');
+    }
+};
